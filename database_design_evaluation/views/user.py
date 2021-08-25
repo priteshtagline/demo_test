29e1b91908eb5addc0289ce37ae5b7ce7d7127c4
Shantilal <shantilal13@gmail.com>
2021-08-25 12:45:33 +0530

    Add only superadmin permissons

---
+++ database_design_evaluation/views/user.py
@@ -0,0 +1,24 @@
+from database_design_evaluation.serializers import *
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+from ..admin_permission import AdminAuthenticationPermission
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          AdminAuthenticationPermission,)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
