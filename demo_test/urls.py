a6aecfc65a39b38602a5ebdda6ede4039656a42d
pritesh <pritesh.taline@gmail.com>
2021-08-26 18:23:53 +0530

    update url path and add migrations file

--- demo_test/urls.py
+++ demo_test/urls.py
@@ -23,8 +23,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 router = routers.DefaultRouter()
-router.register(r'users', views.ProductViewSet)
-# router.register(r'permissions', views.PermissionViewSet, basename=User)
+router.register(r'products', views.ProductViewSet)
 
 
 # Wire up our API using automatic URL routing.
@@ -34,7 +33,7 @@ urlpatterns = [
     path('', include('coding_skill.urls')),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('permissions/<pk>/', views.PermissionViewSet.as_view()),
 ]
